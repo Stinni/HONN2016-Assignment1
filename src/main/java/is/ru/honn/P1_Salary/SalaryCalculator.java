c840933c67dc9a7d9b5de4feaa3b384488c289f8
Stinni <kristinnf13@ru.is>
2016-09-04 17:39:24 +0000

    Part 1 finished, SalaryCalculator and tests are done and commented

--- src/main/java/is/ru/honn/P1_Salary/SalaryCalculator.java
+++ src/main/java/is/ru/honn/P1_Salary/SalaryCalculator.java
@@ -1,11 +1,13 @@
 package is.ru.honn.P1_Salary;
 
 /**
- * Forritið SalaryCalculator (SalaryCalculator.java)
- * prentar út margfeldi af grunnlaunum og unnum tímum eða villu
+ * Hönnun og Smíði Hugbúnaðar - Assignment 1, Part 1:
+ * The program SalaryCalculator (SalaryCalculator.java)
+ * prints out the product of a base salary and worked hours
+ * or prints out an error message and exits the program if input is incorrect
  *
  * @author Kristinn Heiðar Freysteinsson
- * @version 1, 03.09.16
+ * @version 1, 04.09.16
  */
 public class SalaryCalculator {
 
@@ -15,15 +17,20 @@ public class SalaryCalculator {
     private static final int MAX_HOURS = 60;
     // The minimum wage is 500 ISK
     private static final int MINIMUM_WAGE = 500;
-    private static int basePay, hours;
 
     public static void main(String[] args) {
 
+        // The array args has to include at least 2 arguments
         if(args.length < 2) {
             System.out.println("Too few arguments. There has to be 2 integers!");
             System.exit(1);
         }
 
+        int basePay = 0;
+        int hours = 0;
+
+        // Argument strings parsed to integers and if first 2 argumenta aren't integers,
+        // an error message is printed and the program exited
         try {
             basePay = Integer.parseInt(args[0]);
             hours = Integer.parseInt(args[1]);
@@ -36,18 +43,27 @@ public class SalaryCalculator {
         System.out.println(salary);
     }
 
-    private static int calculateSalary(int basePay, int hours) {
+    /**
+     * The function calculateSalary
+     * Calculates the total salary or if the arguments aren't valid
+     * it prints out an error message and exits the program.
+     * @param basePay The base pay used to calculate the total salary
+     * @param hours The number of hours that the employee has worked in a week
+     * @return the total salary
+     */
+    public static int calculateSalary(int basePay, int hours) {
 
-        if(basePay < 500 && hours > MAX_HOURS) {
-            System.out.println("Both arguments are invalid.\n" +
-                    "Base pay can't be lower than the minimum wage (" +
-                    MINIMUM_WAGE + ") and workers aren't allowed to work more than 60 hours per week!");
+        // Checks if the parameters are valid. Print out corresponding error
+        // messages if they're not and exits the program
+        if(basePay < MINIMUM_WAGE && hours > MAX_HOURS) {
+            System.out.println("Both arguments are invalid.\n" + "Base pay can't be lower than the minimum wage (" +
+                    MINIMUM_WAGE + ") and workers aren't allowed to work more than " + MAX_HOURS + " hours per week!");
             System.exit(3);
         } else if(basePay < MINIMUM_WAGE) {
             System.out.println("Base pay can't be lower than the minimum wage (" + MINIMUM_WAGE + ")");
             System.exit(4);
         } else if(hours > MAX_HOURS) {
-            System.out.println("Workers aren't allowed to work more than 60 hours per week!");
+            System.out.println("Workers aren't allowed to work more than " + MAX_HOURS + " hours per week!");
             System.exit(5);
         }
 
